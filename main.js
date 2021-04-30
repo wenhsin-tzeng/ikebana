b5eb0ef998db8cd0b08d0bb81a0024d8990c3166
WenHsin Tzeng <tzenw697@newschool.edu>
2021-04-30 13:07:00 +0800

    intial

---
+++ main.js
@@ -0,0 +1,9 @@
+var flower = document.getElementsByClassName('cropped');
+
+// flower.addEventListener("click", function(event) {
+//   filter: blur(0%);
+// })
+
+document.body.addEventListener("click", function(event){
+  document.location.href = "ikebana.html";
+})
